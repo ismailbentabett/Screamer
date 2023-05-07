6771ab182ec60b5bbc70fbdf04c3174108893116
ismailbentabett <ismailbentabett@gmail.com>
2023-05-07 10:11:26 +0100

    AddApplicationServices

---
+++ Screamer.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Screamer.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper
+            (
+                Assembly.GetExecutingAssembly()
+            );
+
+            services.AddMediatR(
+                cfg => cfg.RegisterServicesFromAssemblies(
+                Assembly.GetExecutingAssembly()
+                ));
+
+            return services;
+        }
+
+    }
+}
