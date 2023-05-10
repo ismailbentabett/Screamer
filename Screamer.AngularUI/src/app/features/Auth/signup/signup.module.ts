b90c0c1e29e3dbe9170fee3d58ec9ee0ef7bf10f
ismailbentabett <ismailbentabett@gmail.com>
2023-05-10 12:50:12 +0100

    lazy loading worked

---
+++ Screamer.AngularUI/src/app/features/Auth/signup/signup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignupRoutingModule } from './signup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SignupRoutingModule
+  ]
+})
+export class SignupModule { }
