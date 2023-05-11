6a59d332cfb106597fb69dd9f4cdb2fa5d105c53
ismailbentabett <ismailbentabett@gmail.com>
2023-05-11 21:22:09 +0100

    user cqrs

---
+++ Screamer.Application/Features/UserRequest/Queries/GetUsersRequest/GetUsersRequestQuery.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Screamer.Application.Dtos;
+
+namespace Screamer.Application.Features.UserRequest.Queries;
+    public record GetUsersRequestQuery : IRequest<List<UserDto>>;
