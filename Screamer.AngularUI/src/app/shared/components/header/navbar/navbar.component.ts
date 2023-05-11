4b37847802b156997860288b68bbdc19be9ceb7e
ismailbentabett <ismailbentabett@gmail.com>
2023-05-11 15:06:27 +0100

    getting that sweet sweet user data

--- Screamer.AngularUI/src/app/shared/components/header/navbar/navbar.component.ts
+++ Screamer.AngularUI/src/app/shared/components/header/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
+import { User } from 'src/app/core/models/User';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
@@ -8,12 +10,16 @@ import { AuthenticationService } from 'src/app/core/services/authentication.serv
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  user: User | null = null;
 
   constructor(
     public authService: AuthenticationService,
     private router: Router
-  ) {}
-
+  ) {
+    this.authService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => (this.user = user as User),
+    });
+  }
 
   logout() {
     this.authService.logout();
