2825ff7008831ed5c941d5665ec097d8094ff13e
ismailbentabett <ismailbentabett@gmail.com>
2023-05-15 20:41:52 +0100

    implimenting messages cqrs pqttren

--- Screamer.Application/Contracts/Exceptions/NotFoundException.cs
+++ Screamer.Application/Contracts/Exceptions/NotFoundException.cs
@@ -8,10 +8,16 @@ namespace Screamer.Application.Contracts.Exceptions
     public class NotFoundException : Exception
     {
         private string userId;
+        private int messageId;
 
         public NotFoundException(string message, string userId) : base(message)
         {
             this.userId = userId;
+        }
+
+        public NotFoundException(string message, int messageId) : base(message)
+        {
+            this.messageId = messageId;
         }
 
         public NotFoundException(string name, object key, object var) : base($"Entity \"{name}\" ({key}) was not found.")
