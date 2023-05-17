b15fea8eb352e22fe9ec3049db2c9a6108c32047
ismailbentabett <ismailbentabett@gmail.com>
2023-05-17 15:02:33 +0100

    color fix

--- Screamer.AngularUI/tailwind.config.js
+++ Screamer.AngularUI/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  content: ["./src/**/*.{html,ts}", "./projects/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dodger-blue": {
+          DEFAULT: "#1DA1F2",
+          50: "#F1F9FE",
+          100: "#DEF1FD",
+          200: "#B7E1FB",
+          300: "#91D1F9",
+          400: "#6AC1F6",
+          500: "#44B1F4",
+          600: "#1DA1F2",
+          700: "#0C82CB",
+          800: "#096096",
+          900: "#063E61",
+          950: "#042D47",
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
