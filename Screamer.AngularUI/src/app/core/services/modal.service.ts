54f662b728d7439d390c84ce536db8e79b827715
ismailbentabett <ismailbentabett@gmail.com>
2023-05-18 13:22:54 +0100

    modal ui

---
+++ Screamer.AngularUI/src/app/core/services/modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  public isOpen: boolean = false;
+  constructor(
+
+  ) {}
+  openPopup() {
+    this.isOpen = true;
+  }
+
+  closePopup() {
+    this.isOpen = false;
+  }
+
+}
