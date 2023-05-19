c7db1a5acb7d5134a847f9b96015b1e58d451dda
ismailbentabett <ismailbentabett@gmail.com>
2023-05-19 17:38:27 +0100

    update and get user stuff

---
+++ Screamer.Application/Dtos/UpdateUserDto.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Screamer.Domain.Entities;
+
+namespace Screamer.Application.Dtos
+{
+    public class UpdateUserDto
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+
+        public string Bio { get; set; }
+        public string Website { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string userName { get; set; }
+
+        public Social Socials { get; set; }
+        public Adress Adress { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
