53eaa1526bc8705ac78660ae9d419e0509cf7800
ismailbentabett <ismailbentabett@gmail.com>
2023-05-29 12:50:30 +0100

    reaction worked

--- Screamer.AngularUI/src/app/shared/components/buttons/reaction-button/reaction-button.component.ts
+++ Screamer.AngularUI/src/app/shared/components/buttons/reaction-button/reaction-button.component.ts
@@ -1,5 +1,12 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, Input } from '@angular/core';
+import { ReactionService } from 'src/app/core/services/reaction.service';
 
 @Component({
   selector: 'app-reaction-button',
@@ -7,28 +14,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./reaction-button.component.scss'],
   animations: [
     trigger('dropdownAnimation', [
-      state('open', style({
-        transform: 'opacity-100 scale-100',
-        opacity: 1,
-      })),
-      state('closed', style({
-        transform: 'opacity-0 scale-95',
-        opacity: 0,
-      })),
+      state(
+        'open',
+        style({
+          transform: 'opacity-100 scale-100',
+          opacity: 1,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'opacity-0 scale-95',
+          opacity: 0,
+        })
+      ),
       transition('closed => open', animate('100ms ease-out')),
       transition('open => closed', animate('75ms ease-in')),
     ]),
   ],
 })
 export class ReactionButtonComponent {
-
-reactions = [
-
-]
-
+  reaction!: string;
+  @Input()
+  postId!: number;
+  constructor(private reactionService: ReactionService) {}
   isOpen = false;
 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
+  }
+
+  react(reactionType: string) {
+    this.reactionService
+      .addPostReaction(this.postId, reactionType)
+      .subscribe(() => {
+        this.reaction = reactionType;
+        this.toggleDropdown();
+      });
   }
 }
