66bee124c3b5c2636e5a488e9d6eb12ab51892b6
ismailbentabett <ismailbentabett@gmail.com>
2023-05-29 23:52:12 +0100

    woow even more

--- Screamer.AngularUI/src/app/shared/components/header/navbar/navbar.component.ts
+++ Screamer.AngularUI/src/app/shared/components/header/navbar/navbar.component.ts
@@ -61,6 +61,24 @@ export class NavbarComponent {
       : 'text-gray-900 hover:bg-gray-50 hover:text-gray-900 rounded-md py-2 px-3 inline-flex items-center text-sm font-medium';
   }
 
+  LinkStyle2(route: string): string {
+    const isActive =
+      this.router.isActive(route, false) || this.router.isActive(route, true);
+
+    return isActive
+      ? 'bg-gray-100 text-gray-900 block rounded-md py-2 px-3 text-base font-medium'
+      : 'bg-white text-gray-500 block rounded-md py-2 px-3 text-base font-medium';
+  }
+
+  MobileLinkStyle(route: string) {
+    const isActive =
+      this.router.isActive(route, false) || this.router.isActive(route, true);
+
+    return isActive
+      ? 'block rounded-md py-2 px-3 text-base font-medium text-gray-500 bg-gray-50 hover:text-gray-900'
+      : 'block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900';
+  }
+
   //oninit
   ngOnInit(): void {
     this.userService
