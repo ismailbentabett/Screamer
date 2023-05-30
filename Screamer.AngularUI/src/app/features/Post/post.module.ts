cb18d2566bc6bfbc9858b2952c8411809eb63618
ismailbentabett <ismailbentabett@gmail.com>
2023-05-30 11:18:14 +0100

    inital show post

--- Screamer.AngularUI/src/app/features/Post/post.module.ts
+++ Screamer.AngularUI/src/app/features/Post/post.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PostRoutingModule } from './post-routing.module';
 import { ShowPostComponent } from './show-post/show-post.component';
 import { EditPostComponent } from './edit-post/edit-post.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ShowPostComponent, EditPostComponent],
-  imports: [CommonModule, PostRoutingModule],
+  imports: [CommonModule, PostRoutingModule, SharedModule],
 })
 export class PostModule {}
--- Screamer.AngularUI/src/app/features/Post/show-post/show-post.component.ts
+++ Screamer.AngularUI/src/app/features/Post/show-post/show-post.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from 'src/app/core/models/Post';
+import { PostService } from 'src/app/core/services/post.service';
 
 @Component({
   selector: 'app-show-post',
   templateUrl: './show-post.component.html',
-  styleUrls: ['./show-post.component.scss']
+  styleUrls: ['./show-post.component.scss'],
 })
 export class ShowPostComponent {
-
+  post!: Post;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postService: PostService
+  ) {
+    router.events.subscribe((val) => {
+      this.route.paramMap.subscribe(async (params) => {
+        let postId = params.get('postId');
+        this.postService
+          .getPost(parseInt(postId as string))
+          .subscribe((post) => {
+            this.post = post as Post;
+          });
+      });
+    });
+  }
 }
