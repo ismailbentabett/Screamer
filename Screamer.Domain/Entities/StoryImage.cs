2b7e5fdfdb84cd2c6e54a9136e7032c30897dc4f
ismailbentabett <ismailbentabett@gmail.com>
2023-06-11 22:54:32 +0100

    delete story

--- Screamer.Domain/Entities/StoryImage.cs
+++ Screamer.Domain/Entities/StoryImage.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Screamer.Domain.Common;
 
 namespace Screamer.Domain.Entities
 {
-    public class StoryImage
+    public class StoryImage : BaseEntity
     {
         public string Url { get; set; }
         public bool IsMain { get; set; }
