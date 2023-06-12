38aac9d3202de6f62f83ac003cf1f501c9346234
ismailbentabett <ismailbentabett@gmail.com>
2023-06-12 01:14:15 +0100

    story fixing

--- Screamer.AngularUI/src/app/shared/components/stories/story/story.component.ts
+++ Screamer.AngularUI/src/app/shared/components/stories/story/story.component.ts
@@ -16,6 +16,7 @@ export class StoryComponent {
   }
 
   loadStories() {
+    // Dummy data array representing stories fetched from the backend
     const stories = [
       {
         escort_id: '1',
@@ -27,75 +28,89 @@ export class StoryComponent {
             url: 'avatar1.jpg',
           },
         },
-        type: 'PIC',
-        upload: {
-          url: 'photo1.jpg',
-        },
-        updated_at: '2023-06-10T10:30:00.000Z',
-        text: 'First story',
-      },
-      {
-        escort_id: '2',
-        escort: {
-          user: {
-            name: 'Jane Smith',
+        stories: [
+          {
+            type: 'PIC',
+            upload: {
+              url: 'photo1.jpg',
+            },
+            updated_at: '2023-06-10T10:30:00.000Z',
+            text: 'First story',
           },
-          profile_picture: {
-            url: 'avatar2.jpg',
+          {
+            type: 'PIC',
+            upload: {
+              url: 'photo1.jpg',
+            },
+            updated_at: '2023-06-10T10:30:00.000Z',
+            text: 'First story',
           },
-        },
-        type: 'VIDEO',
-        upload: {
-          url: 'video1.mp4',
-        },
-        updated_at: '2023-06-10T11:45:00.000Z',
-        text: 'Second story',
+          {
+            type: 'PIC',
+            upload: {
+              url: 'photo1.jpg',
+            },
+            updated_at: '2023-06-10T10:30:00.000Z',
+            text: 'First story',
+          },
+          {
+            type: 'PIC',
+            upload: {
+              url: 'photo1.jpg',
+            },
+            updated_at: '2023-06-10T10:30:00.000Z',
+            text: 'First story',
+          },
+          // Add more stories for this user
+        ],
       },
+      // Add more users with their stories here
     ];
 
-    stories.forEach((story) => {
-      const escortId = story.escort_id;
-      const escortName = story.escort.user.name;
-      const escortAvatar = story.escort.profile_picture
-        ? '/' + story.escort.profile_picture.url
+    stories.forEach((userStory) => {
+      const escortId = userStory.escort_id;
+      const escortName = userStory.escort.user.name;
+      const escortAvatar = userStory.escort.profile_picture
+        ? '/' + userStory.escort.profile_picture.url
         : '/img/defaultAvatar.png';
-      const storyType = story.type === 'PIC' ? 'photo' : 'video';
-      const storyLength = storyType === 'video' ? 0 : 10;
-      const storySrc = '/' + story.upload.url;
-      const storyTime = new Date(story.updated_at).getTime() / 1000;
-      const storyText = story.text ? story.text : '';
 
-      const newStoryItem = this.buildItem(
-        story.escort_id,
-        storyType,
-        storyLength,
-        storySrc,
-        storySrc,
-        escortId,
-        false,
-        storyTime,
-        storyText
-      );
+      const userStories = userStory.stories.map((story) => {
+        const storyType = story.type === 'PIC' ? 'photo' : 'video';
+        const storyLength = storyType === 'video' ? 0 : 10;
+        const storySrc = '/' + story.upload.url;
+        const storyTime = new Date(story.updated_at).getTime() / 1000;
+        const storyText = story.text ? story.text : '';
 
-      let alreadyStory = false;
-      this.orderedStories.forEach((orderedStory: any) => {
-        if (orderedStory.id === escortId) {
-          orderedStory.items.push(newStoryItem);
-          if (orderedStory.lastUpdated > storyTime) {
-            orderedStory.lastUpdated = storyTime;
-          }
-          alreadyStory = true;
-        }
+        return this.buildItem(
+          escortId,
+          storyType,
+          storyLength,
+          storySrc,
+          storySrc,
+          escortId,
+          false,
+          storyTime,
+          storyText
+        );
       });
 
-      if (!alreadyStory) {
+      const existingStory = this.orderedStories.find((s) => s.id === escortId);
+      if (existingStory) {
+        existingStory.items.push(...userStories);
+        const latestStoryTime = Math.max(
+          ...userStories.map((story) => story.time)
+        );
+        if (existingStory.lastUpdated > latestStoryTime) {
+          existingStory.lastUpdated = latestStoryTime;
+        }
+      } else {
         const newStory = {
           id: parseInt(escortId),
           photo: escortAvatar,
           link: '/profile/' + escortId,
-          lastUpdated: storyTime,
+          lastUpdated: Math.max(...userStories.map((story) => story.time)),
           name: escortName,
-          items: [newStoryItem],
+          items: userStories,
         };
         this.orderedStories.push(newStory);
       }
@@ -126,21 +141,111 @@ export class StoryComponent {
     };
   }
 
-  initStories() {
-    const stories = Zuck(
-      document.querySelector('#stories') as HTMLElement,
+  addStory() {
+    const newStory = {
+      escort_id: '1', // Change the escort_id to the current user's ID
+      escort: {
+        user: {
+          name: 'John Doe', // Change the name to the current user's name
+        },
+        profile_picture: {
+          url: 'avatar1.jpg', // Change the URL to the current user's avatar URL
+        },
+      },
+      stories: [
+        {
+          type: 'PIC', // Change the type to 'PIC' for photo or 'VIDEO' for video
+          upload: {
+            url: 'photo2.jpg', // Change the URL to the new story's media URL
+          },
+          updated_at: new Date().toISOString(),
+          text: 'New story', // Change the text to the new story's caption or text
+        },
+      ],
+    };
 
-      {
-        backNative: true,
-        autoFullScreen: false,
-        skin: 'Snapgram',
-        avatars: true,
-        list: false,
-        cubeEffect: true,
-        localStorage: true,
-        stories: this.orderedStories,
-      }
+    // Find the existing user's stories or create a new user entry
+    const existingStory = this.orderedStories.find(
+      (s) => s.id === newStory.escort_id
     );
+
+    if (existingStory) {
+      const storyType = newStory.stories[0].type === 'PIC' ? 'photo' : 'video';
+      const storyLength = storyType === 'video' ? 0 : 10;
+      const storySrc = '/' + newStory.stories[0].upload.url;
+      const storyTime =
+        new Date(newStory.stories[0].updated_at).getTime() / 1000;
+      const storyText = newStory.stories[0].text
+        ? newStory.stories[0].text
+        : '';
+
+      const newStoryItem = this.buildItem(
+        newStory.escort_id,
+        storyType,
+        storyLength,
+        storySrc,
+        storySrc,
+        newStory.escort_id,
+        false,
+        storyTime,
+        storyText
+      );
+
+      existingStory.items.push(newStoryItem);
+      if (existingStory.lastUpdated > storyTime) {
+        existingStory.lastUpdated = storyTime;
+      }
+    } else {
+      const escortId = newStory.escort_id;
+      const escortName = newStory.escort.user.name;
+      const escortAvatar = newStory.escort.profile_picture
+        ? '/' + newStory.escort.profile_picture.url
+        : '/img/defaultAvatar.png';
+
+      const storyType = newStory.stories[0].type === 'PIC' ? 'photo' : 'video';
+      const storyLength = storyType === 'video' ? 0 : 10;
+      const storySrc = '/' + newStory.stories[0].upload.url;
+      const storyTime =
+        new Date(newStory.stories[0].updated_at).getTime() / 1000;
+      const storyText = newStory.stories[0].text
+        ? newStory.stories[0].text
+        : '';
+
+      const newStoryItem = this.buildItem(
+        escortId,
+        storyType,
+        storyLength,
+        storySrc,
+        storySrc,
+        escortId,
+        false,
+        storyTime,
+        storyText
+      );
+
+      const newUserStory = {
+        id: parseInt(escortId),
+        photo: escortAvatar,
+        link: '/profile/' + escortId,
+        lastUpdated: storyTime,
+        name: escortName,
+        items: [newStoryItem],
+      };
+      this.orderedStories.push(newUserStory);
+    }
+  }
+
+  initStories() {
+    const stories = Zuck(document.querySelector('#stories') as any, {
+      backNative: true,
+      autoFullScreen: false,
+      skin: 'Snapgram',
+      avatars: true,
+      list: false,
+      cubeEffect: true,
+      localStorage: true,
+      stories: this.orderedStories,
+    });
     document.body.style.display = 'block';
   }
 }
