d63799ae78b87115d2b659fe2903ada606316701
ismailbentabett <ismailbentabett@gmail.com>
2023-06-15 03:44:44 +0100

    writing full trending service logic

--- Screamer.AngularUI/src/app/core/services/trending.service.ts
+++ Screamer.AngularUI/src/app/core/services/trending.service.ts
@@ -1,40 +1,213 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { PostParams } from '../models/PostParams';
+import { UserParams } from '../models/userParams';
+import {
+  getPaginatedResult,
+  getPaginationHeaders,
+} from '../Helpers/paginationHelper';
+import { map } from 'rxjs';
+import { Post } from '../models/Post';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrendingService {
-  hubUrl = environment.hubUrl;
-
+  baseUrl = environment.baseWebApiUrl;
+  TopPreformingPosts: PostParams | undefined;
+  PostsByHashtag: PostParams | undefined;
+  PostsByCategory: PostParams | undefined;
+  TrendingPosts: PostParams | undefined;
+  TopPreformingUsers: UserParams | undefined;
   constructor(private http: HttpClient) {}
 
   getTopPreformingPost() {
-    return this.http.get(this.hubUrl + 'Post/get-the-top-preforming-post');
+    return this.http.get(this.baseUrl + 'Post/get-the-top-preforming-post');
   }
 
-  getTopPreformingPosts() {
-    return this.http.get(this.hubUrl + 'Post/get-the-top-preforming-posts');
+  getTopPreformingPosts(postParams: PostParams) {
+    let params = getPaginationHeaders(
+      postParams.orderBy.toString(),
+      postParams.userId.toString(),
+
+      postParams.pageNumber,
+      postParams.pageSize
+    );
+    return getPaginatedResult<Post[]>(
+      this.baseUrl + 'Post/get-the-top-preforming-posts',
+      params,
+      this.http
+    ).pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
   }
 
-  getPostsByHashtag() {
-    return this.http.get(this.hubUrl + 'Post/get-posts-by-hashtag');
+  getPostsByHashtag(postParams: PostParams) {
+    let params = getPaginationHeaders(
+      postParams.orderBy.toString(),
+      postParams.userId.toString(),
+
+      postParams.pageNumber,
+      postParams.pageSize
+    );
+    return getPaginatedResult<Post[]>(
+      this.baseUrl + 'Post/get-posts-by-hashtag',
+      params,
+      this.http
+    ).pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
   }
 
-  getPostsByCategory() {
-    return this.http.get(this.hubUrl + 'Post/get-posts-by-category');
+  getPostsByCategory(postParams: PostParams) {
+    let params = getPaginationHeaders(
+      postParams.orderBy.toString(),
+      postParams.userId.toString(),
+
+      postParams.pageNumber,
+      postParams.pageSize
+    );
+    return getPaginatedResult<Post[]>(
+      this.baseUrl + 'Post/get-posts-by-category',
+      params,
+      this.http
+    ).pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
   }
 
-  getTrendingPosts() {
-    return this.http.get(this.hubUrl + 'Post/get-trending-posts');
+  getTrendingPosts(postParams: PostParams) {
+    let params = getPaginationHeaders(
+      postParams.orderBy.toString(),
+      postParams.userId.toString(),
+
+      postParams.pageNumber,
+      postParams.pageSize
+    );
+    return getPaginatedResult<Post[]>(
+      this.baseUrl + 'Post/get-trending-posts',
+      params,
+      this.http
+    ).pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
   }
 
   getTopPreformingUser() {
-    return this.http.get(this.hubUrl + 'User/top-preforming-user');
+    return this.http.get(this.baseUrl + 'User/top-preforming-user');
   }
 
-  getTopPreformingUsers() {
-    return this.http.get(this.hubUrl + 'User/top-preforming-users');
+  getTopPreformingUsers(userParams: UserParams) {
+    let params = getPaginationHeaders(
+      userParams.orderBy.toString(),
+      userParams.userId.toString(),
+
+      userParams.pageNumber,
+      userParams.pageSize
+    );
+    return getPaginatedResult<User[]>(
+      this.baseUrl + 'User/top-preforming-users',
+      params,
+      this.http
+    ).pipe(
+      map((response: any) => {
+        return response;
+      })
+    );
+  }
+
+  getTopPreformingPostsParams(
+    userId: string,
+    pageSize: number,
+    pageNumber: number
+  ) {
+    this.TopPreformingPosts = new PostParams();
+    this.TopPreformingPosts.orderBy = 'CreatedAt';
+    this.TopPreformingPosts.pageNumber = pageNumber;
+    this.TopPreformingPosts.pageSize = pageSize;
+    this.TopPreformingPosts.userId = userId;
+
+    return this.TopPreformingPosts;
+  }
+
+  setTopPreformingPostsParams(params: PostParams) {
+    this.TopPreformingPosts = params;
+  }
+
+  getPostsByHashtagParams(
+    userId: string,
+    pageSize: number,
+    pageNumber: number
+  ) {
+    this.PostsByHashtag = new PostParams();
+    this.PostsByHashtag.orderBy = 'CreatedAt';
+    this.PostsByHashtag.pageNumber = pageNumber;
+    this.PostsByHashtag.pageSize = pageSize;
+    this.PostsByHashtag.userId = userId;
+
+    return this.PostsByHashtag;
+  }
+
+  setPostsByHashtagParams(params: PostParams) {
+    this.PostsByHashtag = params;
+  }
+
+  getPostsByCategoryParams(
+    userId: string,
+    pageSize: number,
+    pageNumber: number
+  ) {
+    this.PostsByCategory = new PostParams();
+    this.PostsByCategory.orderBy = 'CreatedAt';
+    this.PostsByCategory.pageNumber = pageNumber;
+    this.PostsByCategory.pageSize = pageSize;
+    this.PostsByCategory.userId = userId;
+
+    return this.PostsByCategory;
+  }
+
+  setPostsByCategoryParams(params: PostParams) {
+    this.PostsByCategory = params;
+  }
+
+  getTrendingPostsParams(userId: string, pageSize: number, pageNumber: number) {
+    this.TrendingPosts = new PostParams();
+    this.TrendingPosts.orderBy = 'CreatedAt';
+    this.TrendingPosts.pageNumber = pageNumber;
+    this.TrendingPosts.pageSize = pageSize;
+    this.TrendingPosts.userId = userId;
+
+    return this.TrendingPosts;
+  }
+
+  setTrendingPostsParams(params: PostParams) {
+    this.TrendingPosts = params;
+  }
+
+  getTopPreformingUsersParams(
+    userId: string,
+    pageSize: number,
+    pageNumber: number
+  ) {
+    this.TopPreformingUsers = new UserParams();
+    this.TopPreformingUsers.orderBy = 'CreatedAt';
+    this.TopPreformingUsers.pageNumber = pageNumber;
+    this.TopPreformingUsers.pageSize = pageSize;
+
+    return this.TopPreformingUsers;
+  }
+
+  setTopPreformingUsersParams(params: UserParams) {
+    this.TopPreformingUsers = params;
   }
 }
