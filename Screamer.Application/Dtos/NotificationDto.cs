240663f2daa4d0e565a451175a3e3037abf16a86
ismailbentabett <ismailbentabett@gmail.com>
2023-06-18 16:10:37 +0100

    more initiation

---
+++ Screamer.Application/Dtos/NotificationDto.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Screamer.Application.Dtos
+{
+    public class NotificationDto
+    {
+             public string Message { get; set; }
+
+        public string Type { get; set; }
+
+        public int ChatRoomId { get; set; }
+        public int NotificationRoomId { get; set; }
+
+        public int PostId { get; set; }
+
+        public string senderId { get; set; }
+        public string recieverId { get; set; }
+
+        public int CommentId { get; set; }
+
+        public int ReplyId { get; set; }
+
+        public int ReactionId { get; set; }
+
+        public int TagId { get; set; }
+
+        public int MentionId { get; set; }
+
+        public bool IsRead { get; set; }   
+    }
+}
